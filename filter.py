46a312030ccbc593c8f4db409e64365ae77fa69f
Rachelli-pixel <zixuanli@umich.edu>
2026-04-07 19:37:35 -0400

    Add files via upload

--- filter.py
+++ filter.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob
 
 # Step 1: Get a list of all MLS sold CSV files in the folder
-file_list = glob.glob('raw/CRMLSSold2024*.csv')  # all 2024 monthly files
+file_list = glob.glob('raw/*Sold*.csv')
 
 # Step 2: Read each CSV into a DataFrame and store in a list
 dfs = [pd.read_csv(file) for file in file_list]
@@ -12,9 +12,9 @@ sold = pd.concat(dfs, ignore_index=True)
 
 # Step 4: Optional filtering and cleaning
 sold = sold[sold['PropertyType'] == 'Residential']
-# sold = sold.dropna()  # if you want to drop rows with missing values
+# sold = sold.dropna()
 
 # Step 5: Save combined dataset
-sold.to_csv('filtered/CRMLSSold_Residential_2024.csv', index=False)
+sold.to_csv('filtered/CRMLSSold_Residential.csv', index=False)
 
 print("Combined CSV saved. Number of rows:", len(sold))
--- filter_listing.py
+++ filter_listing.py
@@ -2,19 +2,19 @@ import pandas as pd
 import glob
 
 # Step 1: Get a list of all MLS sold CSV files in the folder
-file_list = glob.glob('raw/CRMLSListing2024*.csv')  # all 2024 monthly files
+file_list = glob.glob('raw/*Listing*.csv')
 
 # Step 2: Read each CSV into a DataFrame and store in a list
 dfs = [pd.read_csv(file) for file in file_list]
 
 # Step 3: Concatenate all DataFrames into one
-sold = pd.concat(dfs, ignore_index=True)
+Listing = pd.concat(dfs, ignore_index=True)
 
 # Step 4: Optional filtering and cleaning
-sold = sold[sold['PropertyType'] == 'Residential']
-# sold = sold.dropna()  # if you want to drop rows with missing values
+Listing = Listing[Listing['PropertyType'] == 'Residential']
+# Listing = Listing.dropna()
 
 # Step 5: Save combined dataset
-sold.to_csv('filtered/CRMLSListing_Residential_2024.csv', index=False)
+Listing.to_csv('filtered/CRMLSListing_Residential.csv', index=False)
 
-print("Combined CSV saved. Number of rows:", len(sold))
+print("Combined CSV saved. Number of rows:", len(Listing))
