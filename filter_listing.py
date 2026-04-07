7aa1d651b35f1291833d6474f0a74704383c62cd
Rachelli-pixel <zixuanli@umich.edu>
2026-04-07 19:26:12 -0400

    Add files via upload

--- filter_listing.py
+++ filter_listing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob
 
 # Step 1: Get a list of all MLS sold CSV files in the folder
-file_list = glob.glob('raw/CRMLSSold2024*.csv')  # all 2024 monthly files
+file_list = glob.glob('raw/CRMLSListing2024*.csv')  # all 2024 monthly files
 
 # Step 2: Read each CSV into a DataFrame and store in a list
 dfs = [pd.read_csv(file) for file in file_list]
@@ -15,6 +15,6 @@ sold = sold[sold['PropertyType'] == 'Residential']
 # sold = sold.dropna()  # if you want to drop rows with missing values
 
 # Step 5: Save combined dataset
-sold.to_csv('filtered/CRMLSSold_Residential_2024.csv', index=False)
+sold.to_csv('filtered/CRMLSListing_Residential_2024.csv', index=False)
 
 print("Combined CSV saved. Number of rows:", len(sold))
